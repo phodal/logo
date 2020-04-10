42e4994ee598cde7e872b7395e49ec17c53f523d
Phodal Huang <h@phodal.com>
2020-04-10 09:49:51 +0800

    docs: add resized images

--- index.js
+++ index.js
@@ -2,27 +2,49 @@ var elements = require('./elements.json');
 var fs = require('fs');
 var request = require('request');
 
+function downloadFromOriginImages() {
+    for (let i = 0; i < elements.elements.length; i++) {
+        var element = elements.elements[i];
+        let newName = element.name.toLowerCase().replace(/\s/g, '-')
+        if (!element.image) {
+            continue;
+        }
+
+        var download = function (uri, filename, callback) {
+            request.head(uri, function (err, res, body) {
+                if ('image/jpeg' !== res.headers['content-type']) {
+                    return;
+                }
+                console.log('content-type:', res.headers['content-type']);
+                console.log('content-length:', res.headers['content-length']);
+
+                request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+            });
+        };
+
+        download(element.image, 'images/' + newName.replace(/\//g, '') + '.jpg', function () {
+            console.log('done');
+        });
+    }
+}
+
+// downloadFromOriginImages();
+
 for (let i = 0; i < elements.elements.length; i++) {
     var element = elements.elements[i];
     let newName = element.name.toLowerCase().replace(/\s/g, '-')
-    if (!element.image) {
-        continue;
-    }
 
     var download = function (uri, filename, callback) {
         request.head(uri, function (err, res, body) {
-            if ('image/jpeg' !== res.headers['content-type']) {
+            if ('image/png' !== res.headers['content-type']) {
                 return;
             }
-            console.log('content-type:', res.headers['content-type']);
-            console.log('content-length:', res.headers['content-length']);
 
             request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
         });
     };
 
-    download(element.image, 'images/' + newName.replace(/\//g, '') + '.jpg', function () {
+    download('https://xebialabs.com/wp-content/uploads/files/logos/resized/' + newName + '.png', 'png/' + newName.replace(/\//g, '') + '.png', function () {
         console.log('done');
     });
 }
-
